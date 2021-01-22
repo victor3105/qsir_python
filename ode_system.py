9589e9ef9b9b8e497bbdce08c8e1362eae785f24
Viktor Glebov <glebov_v94@mail.ru>
2021-01-21 17:04:55 -0700

    Tuned hyperparameters of genetic algorithm

--- ode_system.py
+++ ode_system.py
@@ -13,6 +13,7 @@ from arizona_data import INFECTED, RECOVERED, DEAD, ALPHA, BETA, GAMMA
 model = Sequential()
 model.add(Dense(10, input_dim=3, activation='relu'))
 model.add(Dense(1))
+# contains S, I, R, Q values in rows
 predicted = np.ones((4, 66))
 
 
@@ -21,8 +22,12 @@ def my_loss(y_true, y_pred):
     recovered = RECOVERED
     dead = DEAD
     pred = predicted
-    loss = np.sum((np.log(infected) - np.log(pred[1][:] + pred[3][:]))**2)
-    loss += np.sum((np.log(recovered + dead) - np.log(pred[2][:]))**2)
+    # get rid of negative values
+    pred = np.where(pred < 0, 1e9, pred)
+    loss = np.sum((np.log(infected) - np.log((pred[1][:] + pred[3][:])))**2)
+    loss += np.sum((np.log(recovered + dead) - np.log((pred[2][:])))**2)
+    print(f'Loss: {loss}')
+    print()
     return loss
 
 
@@ -41,9 +46,6 @@ def fitness_func(solution, sol_idx):
     solution_fitness = 1.0 / loss
 
     return solution_fitness
-
-# model.compile(loss=my_loss, optimizer='adam', metrics=['accuracy'])
-# model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
 
 def train_ode(model, epochs, fun, t_span, y0, method='RK45', t_eval=None, dense_output=False,
@@ -71,7 +73,7 @@ delta = 0.01
 def dxdt_new(t, x, *args):
     N, beta, gamma, delta = args
     deltaInfected = beta * x[0] * x[1] / N
-    quarantine = model.predict(np.expand_dims(x[:3], axis=0)) / N
+    quarantine = model.predict(np.expand_dims(x[:3], axis=0))[0][0] * x[1] / (N * 100)
     recoveredQ = delta * x[3]
     recoveredNoQ = gamma * x[1]
     S = -deltaInfected
@@ -89,8 +91,8 @@ x0 = S0, I0, R0, Q0
 
 weights_vector = pygad.kerasga.model_weights_as_vector(model=model)
 keras_ga = pygad.kerasga.KerasGA(model=model,
-                                 num_solutions=10)
-num_generations = 71
+                                 num_solutions=30)
+num_generations = 200
 num_parents_mating = 5
 initial_population = keras_ga.population_weights
 
@@ -101,13 +103,19 @@ ga_instance = pygad.GA(num_generations=num_generations,
 ga_instance.run()
 ga_instance.plot_result(title="PyGAD & Keras - Iteration vs. Fitness", linewidth=4)
 
-t = np.linspace(0, 100, 100)
-sol = solve_ivp(dxdt_new, (0, 100.0), [S0, I0, R0, Q0], method='RK45', args=(N0, beta, gamma, delta), t_eval=t)
+t = np.linspace(0, 66, 66)
+sol = solve_ivp(dxdt_new, (0, 66.0), [S0, I0, R0, Q0], method='RK45', args=(N0, beta, gamma, delta), t_eval=t)
 
 plt.plot(t, sol.y[0], 'r', label='S')
 plt.plot(t, sol.y[1], 'g', label='I')
 plt.plot(t, sol.y[2], 'b', label='R')
 plt.plot(t, sol.y[3], 'k', label='Q')
+plt.legend(loc='best')
+plt.grid()
+plt.show()
+
+plt.plot(t, sol.y[1], 'r', label='predicted')
+plt.plot(t, INFECTED, 'g', label='real')
 plt.legend(loc='best')
 plt.grid()
 plt.show()
